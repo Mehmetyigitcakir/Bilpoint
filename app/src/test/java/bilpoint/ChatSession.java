6a70ee4e455ff46d5aec3c9b252267af92d98d76
nazimyavuz <onnumarahesapke@gmail.com>
2026-04-01 14:48:27 +0300

    Event Class and classes related to it

--- app/src/test/java/bilpoint/ChatSession.java
+++ app/src/test/java/bilpoint/ChatSession.java
@@ -1,5 +1,18 @@
 package bilpoint;
 
-public class ChatSession {
+public class ChatSession extends Event{
+
+    protected String sessionId;
+    protected String eventId;
+    protected int expirationTime;
+
+    public ChatSession(String eventId, String hostId, String title, String location) {
+        super(eventId, hostId, title, location);
+        //TODO Auto-generated constructor stub
+    }
+
+    public void clearHistory(){
+        
+    }
     
 }
